c0c0802b1a4f005b95fe2c40c39313b147a319bc
qq937043791 <55073498+qq937043791@users.noreply.github.com>
2019-09-16 15:32:37 +0800

    Update shouxietishibie_2.py

--- MachineLearning/shouxietishibie_2.py
+++ MachineLearning/shouxietishibie_2.py
@@ -68,17 +68,6 @@ def bias_variable(shape):
 # 池化用最简单传统的2×2大小的模板做max pooling --主要操作是保留特征的完整性进行降维操作以及减少参数：如进行 旋转、平移、缩小
 
 
-# tf.nn.conv2d(input,filter,strides,padding,use_cudnn_on_gpu=Nne)
-# 1、input：输入的要做卷积的图片，要求为1个张量shape [batch, in_height, in_weight, in_channel]，其中batch为图片的数量，in_height为图片的高度
-# in_weight 为图片宽度，in_channel 为图片的通道数，灰度图该值为1，彩色图为3。
-# 2、filter： 卷积核，要求也是一个张量，shape为 [ filter_height, filter_weight, in_channel, out_channels ]，
-# 其中 filter_height 为卷积核高度，filter_weight 为卷积核宽度，in_channel 是图像通道数 ，和 input 的 in_channel 要保持一致，
-# out_channel 是卷积核数量。
-# 3、strides： 卷积时在图像每一维的步长，这是一个一维的向量，[ 1, strides, strides, 1]，第一位和最后一位固定必须是1
-# 4、padding： string类型，值为“SAME” 和 “VALID”，表示的是卷积的形式，是否考虑边界。
-# "SAME"是考虑边界，不足的时候用0去填充周围，"VALID"则不考虑
-# 5、use_cudnn_on_gpu： bool类型，是否使用cudnn加速，默认为true
-
 #卷积
 # tf.nn.conv2d(input,filter,strides,padding,use_cudnn_on_gpu=Nne)
 # 1、input：输入的要做卷积的图片，要求为1个张量shape [batch, in_height, in_weight, in_channel]，其中batch为图片的数量，in_height为图片的高度
